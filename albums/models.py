14adb2c55f8bd8d1e0a7086fcf8212e03238b305
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-10-23 17:02:51 +0200

    models str update

--- albums/models.py
+++ albums/models.py
@@ -5,3 +5,6 @@ class Album(models.Model):
     title = models.CharField(max_length=50)
     artist = models.CharField(max_length=50)
     cover_image = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.title} by {self.artist} ({self.id})"
