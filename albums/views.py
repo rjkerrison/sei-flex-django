dec65004154631af0026c0d576b9256c5be5e5df
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-10-23 13:55:25 +0200

    REST first attempt

--- albums/views.py
+++ albums/views.py
@@ -5,6 +5,8 @@ from .models import Album
 
 # Create your views here.
 def index(request):
+    print(request)
+
     # grabbing what we need from database
     list = Album.objects.all
     # creating a context object
@@ -12,3 +14,46 @@ def index(request):
 
     # rendering based on a template
     return render(request, "albums/index.html", context)
+
+
+# API handlers
+def albums(request):
+    if request.method == "GET":
+        return read(request)
+    if request.method == "POST":
+        return create(request)
+
+
+def album(request):
+    if request.method == "GET":
+        return read_one(request)
+    if request.method == "PATCH":
+        return update(request)
+    if request.method == "DELETE":
+        return delete(request)
+
+
+# CRUD TIME
+def create(request):
+    # something something
+    return HttpResponse("CREATE")
+
+
+def read(request):
+    # can we get albums back as json?
+    return HttpResponse("READ")
+
+
+def read_one(request):
+    # can we get one album back as json?
+    return HttpResponse("READ ONE")
+
+
+def update(request):
+    # can we update an album
+    return HttpResponse("UPDATE")
+
+
+def delete(request):
+    # can we delete an album
+    return HttpResponse("DELETE")
