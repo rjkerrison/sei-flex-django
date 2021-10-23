52e85c455832f7e8339b8294bc3e34459ce0791c
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-10-23 11:46:59 +0200

    adding album model

--- albums/models.py
+++ albums/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+    title = models.CharField(max_length=50)
+    artist = models.CharField(max_length=50)
+    cover_image = models.CharField(max_length=200)
