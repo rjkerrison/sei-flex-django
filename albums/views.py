f2aeea92f567b3ee33decee1011bd730bb673957
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-10-23 12:40:33 +0200

    album migrations and views

--- albums/views.py
+++ albums/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Album
 
 # Create your views here.
+def index(request):
+    # grabbing what we need from database
+    list = Album.objects.all
+    # creating a context object
+    context = {"albums": list}
+
+    # rendering based on a template
+    return render(request, "albums/index.html", context)
