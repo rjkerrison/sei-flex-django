70e8b6f4cbe6a4563d41241c5ea2009fb5bbdd75
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-10-23 16:49:34 +0200

    views

--- albums/urls.py
+++ albums/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, AlbumListView
+from .views import index, AlbumListView, AlbumDetailView
 
 urlpatterns = [
     path("", index),
     path("api/", AlbumListView.as_view()),
+    path("api/<int:id>/", AlbumDetailView.as_view()),
 ]
--- albums/views.py
+++ albums/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from rest_framework import views, response, status
+from rest_framework import views, response, status, exceptions
 
 from .models import Album
 from .serializers import AlbumSerializer
@@ -34,4 +34,34 @@ class AlbumListView(views.APIView):
 
         return response.Response(
             album_to_add.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class AlbumDetailView(views.APIView):
+    def get_album_by_id(self, id):
+        try:
+            return Album.objects.get(id=id)
+        except Album.DoesNotExist:
+            raise exceptions.NotFound(detail="Album does not exist")
+
+    def get(self, request, id):
+        album = self.get_album_by_id(id)
+        serialized_album = AlbumSerializer(album)
+        return response.Response(serialized_album.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, id):
+        album = self.get_album_by_id(id)
+        album.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, id):
+        album = self.get_album_by_id(id)
+        updated_album = AlbumSerializer(album, data=request.data)
+        if updated_album.is_valid():
+            updated_album.save()
+            return response.Response(
+                updated_album.data, status=status.HTTP_202_ACCEPTED
+            )
+        return response.Response(
+            updated_album.errors, status=status.HTTP_400_BAD_REQUEST
         )
