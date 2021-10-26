d4cf1acd2058574e6852bc83423cbb614ae8c534
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-10-26 19:54:48 +0200

    artist-album relationship

--- albums/models.py
+++ albums/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from artists.models import Artist
 
 # Create your models here.
 class Album(models.Model):
     title = models.CharField(max_length=50)
     artist = models.CharField(max_length=50)
     cover_image = models.CharField(max_length=200)
+    album_artist = models.ForeignKey(
+        Artist, on_delete=models.CASCADE, null=True, default=None
+    )
 
     def __str__(self):
         return f"{self.title} by {self.artist} ({self.id})"
