9326a86c1dbf34359adeed1b3618e0ab6ac51851
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-10-26 19:42:45 +0200

    artists

--- artists/admin.py
+++ artists/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Artist, Member
 
 # Register your models here.
+admin.site.register(Artist)
+admin.site.register(Member)
--- artists/models.py
+++ artists/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=50, default=None)
+    members = models.ManyToManyField("Member", related_name="artists", blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Member(models.Model):
+    name = models.CharField(max_length=50)
+    date_of_birth = models.DateField(max_length=50)
+
+    def __str__(self):
+        return self.name
