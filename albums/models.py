043d15a2be4c97b8757860363e982a5dbdeb1667
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-10-26 19:59:57 +0200

    renaming album_artist to artist

--- albums/models.py
+++ albums/models.py
@@ -5,9 +5,9 @@ from artists.models import Artist
 class Album(models.Model):
     title = models.CharField(max_length=50)
     cover_image = models.CharField(max_length=200)
-    album_artist = models.ForeignKey(
+    artist = models.ForeignKey(
         Artist, on_delete=models.CASCADE, null=True, default=None
     )
 
     def __str__(self):
-        return f"{self.title} by {self.album_artist.name} ({self.id})"
+        return f"{self.title} by {self.artist.name} ({self.id})"
