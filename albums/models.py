1c6bacb88fc87818bac748490ee3870929ebb4ef
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2021-10-26 19:57:11 +0200

    remove original artist field

--- albums/models.py
+++ albums/models.py
@@ -4,11 +4,10 @@ from artists.models import Artist
 # Create your models here.
 class Album(models.Model):
     title = models.CharField(max_length=50)
-    artist = models.CharField(max_length=50)
     cover_image = models.CharField(max_length=200)
     album_artist = models.ForeignKey(
         Artist, on_delete=models.CASCADE, null=True, default=None
     )
 
     def __str__(self):
-        return f"{self.title} by {self.artist} ({self.id})"
+        return f"{self.title} by {self.album_artist.name} ({self.id})"
